5de5fceeefebd98682713b132834b5af59cf4937
SumanSinha1194 <suman@innolat.com>
2017-05-05 12:14:03 +0530

    Template added in repository
    Template added in repository

---
+++ EILabs/app/Home/homeController.js
@@ -0,0 +1,7 @@
+﻿'use strict';
+app.controller('homeController', ['$scope', '$rootScope', function ($scope, $rootScope) {
+
+    $(document).ready(function () {
+        $(this).scrollTop(0);
+    });
+}]);
---
+++ EILabs/app/app.js
@@ -0,0 +1,20 @@
+﻿var app = angular.module('EILabsWebApp', ['ngRoute', 'ui.bootstrap']);
+
+app.constant('EILabsWEB_ENDPOINT_URI', 'http://localhost:54383/')
+
+
+app.config(function ($routeProvider) {
+    /*Start Web Routes*/
+
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "app/Home/home.html"
+    });
+
+
+    $routeProvider.otherwise({ redirectTo: "/home" });
+});
+/*End Web Routes*/
+
+
+
